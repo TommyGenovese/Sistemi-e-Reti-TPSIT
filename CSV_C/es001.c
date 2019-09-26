2e171193966c0fd46b9410b20fcb7d0e52666e43
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2019-09-26 11:48:36 +0200

    close to the end of exercise 001

--- CSV_C/es001.c
+++ CSV_C/es001.c
@@ -3,6 +3,7 @@
 #include<string.h>
 
 #define DELIM ","
+#define BSIZE 1024
 #define NGAMES 16600    //numero di giochi massimi
 #define NAME 30     //numero di caratterri per il nome del gioco
 const char SEP = ','; //carattere separatore
@@ -10,11 +11,11 @@ const char SEP = ','; //carattere separatore
 //definisco la struttura
     typedef struct game{
         int rank;
-        char name[NAME];
-        char platform[NAME];
+        char* name[NAME];
+        char* platform[NAME];
         int year;
-        char genre[NAME];
-        char publisher[NAME];
+        char* genre[NAME];
+        char* publisher[NAME];
         float NA_Sales;
         float EU_Sales;
         float JP_Sales;
@@ -24,6 +25,7 @@ const char SEP = ','; //carattere separatore
 
 //prototipi
 void loadTabFromFile(char n[], Game list[]);
+void visualizeInfoGame(Game list[]);
 
 int main(){
     //definizione di variabili
@@ -33,37 +35,48 @@ int main(){
 
     //carico la tabella da file
     loadTabFromFile(fileN,list);
+    visualizeInfoGame(list);
 
 }
 
 
 void loadTabFromFile(char n[], Game list[]){
-    char c;
-    char forwhile[NAME];
     FILE *fp;
-    int k=0;
+    int k=1;
+    char buffer[NAME];
 
     fp = fopen(n,"r");
 
     if(fp==NULL){
         printf("File %s inesistente\n" , n);
-        getch();
     }else{
-        while(fgets(fp, "s", list)!=NULL){
-            list[k].rank=atoi(strtok(list, DELIM));
-            list[k].name[NAME]=atoi(strtok(NULL, DELIM));
-            list[k].platform[NAME]=atoi(strtok(NULL, DELIM));
-            list[k].year=atoi(strtok(NULL, DELIM));
-            list[k].genre[NAME]=atoi(strtok(NULL, DELIM));
-            list[k].publisher[NAME]=atoi(strtok(NULL, DELIM));
-            list[k].NA_Sales=atoi(strtok(NULL, DELIM));
-            list[k].EU_Sales=atoi(strtok(NULL, DELIM));
-            list[k].JP_Sales=atoi(strtok(NULL, DELIM));
-            list[k].Other_Sales=atoi(strtok(NULL, DELIM));
-            list[k].Global_Sales=atoi(strtok(NULL, DELIM));
-        }
-
+        //inizio il ciclo per la copiatura del file in un vettore di strutture
+        //while(fgets(buffer, BSIZE, fp)){
+            if(k==0){
+                k=0;   //salto la prima riga
+            }else{
+                list[k].rank = atoi(strtok(buffer, DELIM));
+                list[k].name[NAME] = strtok(NULL, DELIM);
+                list[k].platform[NAME] = strtok(NULL, DELIM);
+                list[k].year = atoi(strtok(NULL, DELIM));
+                list[k].genre[NAME] = strtok(NULL, DELIM);
+                list[k].publisher[NAME] = strtok(NULL, DELIM);
+                list[k].NA_Sales = atoi(strtok(NULL, DELIM));
+                list[k].EU_Sales = atoi(strtok(NULL, DELIM));
+                list[k].JP_Sales = atoi(strtok(NULL, DELIM));
+                list[k].Other_Sales = atoi(strtok(NULL, DELIM));
+                list[k].Global_Sales = atoi(strtok(NULL, DELIM));
+                k++;
+                printf("%s", list[k].name);
+            }
+       // }
     }
-
     fclose(fp);
+}
+
+void visualizeInfoGame(Game list[]){
+    int n;
+    printf("inserisci il numero del gioco");
+    scanf("%d", n);
+    printf("il nome del giorco è:%s, è per %s, uscito nel %d, di genere %s, dalla %s", list[n].name, list[n].platform, list[n].year, list[n].genre, list[n].publisher);
 }
