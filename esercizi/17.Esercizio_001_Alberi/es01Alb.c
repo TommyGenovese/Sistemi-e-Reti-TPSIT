664e994dad52da21a4975b7b72586b81272de8be
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2020-02-05 12:00:34 +0100

    .

--- esercizi/17.Esercizio_001_Alberi/es01Alb.c
+++ esercizi/17.Esercizio_001_Alberi/es01Alb.c
@@ -1,18 +1,76 @@
+//visualizzare un albero e inserire un elemento
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+/* definizione di albero */
+typedef struct nodo {
+  	double radice;
+  	struct nodo *sx;
+	struct nodo *dx;
+}NODO;
+typedef struct NODO *Albero;
 
-void insert(struct tree_node **tree, struct tree_nodes* new){
-	if(*tree == NULL){
-		*tree= new;
-        (*tree)-> left = NULL;
+Albero costruisci(Albero,int);
+void StampaAlbero(Albero);
+
+/*void insert(struct tree_node **tree, struct tree_nodes *new)
+{
+	if (*tree == NULL)
+	{
+		*tree = new;
+		(*tree)->left = NULL;
 		(*tree)->right = NULL;
-	}else{
-		if(new->key < (*tree)->key){
-			Insert(&(*tree)->left, new);
-		}else if(new->key > (*tree)->key){
-			Insert(&(*tree)->right, new);
-		}else
-			Printf("Chiave duplicata\n");
 	}
+	else
+	{
+		if (new->key < (*tree)->key)
+		{
+			Insert(&(*tree)->left, new);
+		}
+		else if (new->key > (*tree)->key)
+		{
+			Insert(&(*tree)->right, new);
+		}
+		else
+			printf("Chiave duplicata\n");
+	}
+}*/
+
+Albero costruisci(Albero a,int n){
+        srand(time(NULL));
+        int i;
+        if(n==0)
+          a=NULL;
+        else{
+          for(i=1;i<=n;i++)     
+          a=(NODO*)malloc(sizeof(NODO));
+          (*a)->radice=(rand()%20)-20;
+          a->sx->radice=(a->sx,i);
+          a->dx->radice=(a->dx,i);
+          }
+        return a;
+} 
+
+// stampa in ordine dell'albero a
+void StampaAlbero(Albero a) {
+  if(a==NULL) {
+    printf("()");
+    return;
+  }
+
+  printf("( %d ", a->radice);
+  StampaAlbero(a->sx);
+  StampaAlbero(a->dx);
+  printf(" ) ");
+}
+
+int main(){
+    Albero a;
+    int n,i;
+    printf("Quanti elementi devono essere inseriti?\n");
+    scanf("%d",&n);
+    a=costruisci(a,n);
+    StampaAlbero(a);
+    // system("pause");
 }
