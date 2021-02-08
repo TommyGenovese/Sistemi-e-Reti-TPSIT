212e6299b805c2738d9d0f9f3052992a63c9f46a
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2021-02-08 19:20:00 +0100

    AlphaBot_flask

---
+++ TPSIT_5A/HTTP/AlphaBot_flask/Server.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, url_for, request
+import time, alphabot
+#add log
+app = Flask(__name__)
+
+robot = alphabot.AlphaBot()
+robot.stop()
+
+@app.route('/drive', methods=['POST'])
+def drivep():
+    print("Start")
+    #lettura dati dal sito
+    movement = request.form['btn']
+    if movement == "Forward":
+        print("avanti")
+        robot.forward()
+        time.sleep(0.5)
+        robot.stop()
+    if movement == "Right":
+        print("Sinistra")
+        robot.left()
+        time.sleep(0.5)
+        robot.stop()
+    if movement == "Backward":
+        print("Indietro")
+        robot.backward()
+        time.sleep(0.5)
+        robot.stop()
+    if movement == "Left":
+        print("Destra")
+        robot.right()
+        time.sleep(0.5)
+        robot.stop()
+    return render_template("drive.html", methods=['GET'])
+
+@app.route("/drive", methods=['GET'])
+def drive():
+    print("Drive")
+    return render_template("drive.html")
+
+@app.route("/")             #http://127.0.0.1:5000/
+def main():
+    print("Main")
+    robot.stop()
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", debug=True)
